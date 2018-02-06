96821bbcd4dcc558066cad02bc93f9232a6be5a8
Matin <36054089+matinkahi@users.noreply.github.com>
2018-02-06 20:08:01 +0330

    Update api.sh

--- api.sh
+++ api.sh
@@ -4,4 +4,4 @@ killall nohup
 killall .telegram-cli
 killall apimode
 rm -rf /root/open/.telegram-cli/apimode
-./tg -s ./bot.lua -p apimode --bot=TOKEN
+./tg -s ./bot.lua -p apimode --bot=522141789:AAE6Y-8T8KQyckome9x6NqPH93pqXIibh6M
